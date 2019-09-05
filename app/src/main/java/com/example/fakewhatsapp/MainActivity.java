c6960f48904e6bf5561599e5ccb1b40544c33a6f
Per77070 <per.gustafsson77@outlook.com>
2019-09-05 13:12:44 +0200

    Firts commit

---
+++ app/src/main/java/com/example/fakewhatsapp/MainActivity.java
@@ -0,0 +1,36 @@
+package com.example.fakewhatsapp;
+
+import android.os.Build;
+import android.support.annotation.RequiresApi;
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+
+public class MainActivity extends AppCompatActivity {
+
+   private Toolbar mToolbar;
+    private ViewPager myViewPager;
+    private TabLayout myTabLayout;
+
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+mToolbar = (Toolbar) findViewById(R.id.main_page_toolbar);
+setSupportActionBar(mToolbar);
+
+
+
+
+getSupportActionBar().setTitle("WhatsApp");
+
+
+    }
+}
